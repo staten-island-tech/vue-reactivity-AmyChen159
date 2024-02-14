22569a415616c02518cc79c20ab1159d1230c8f0
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-02-14 09:21:57 -0500

    whgjtrewjhgfdSAD

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,3 +1,5 @@
+//https://www.strandbooks.com/metaList/bestsellers
+
 <template>
   <div>
 
@@ -5,7 +7,128 @@
 </template>
 
 <script setup>
-const balls = []
+const balls = [
+  {
+    title: "Everything I Know About Love: A Memoir",
+    author: "Dolly Alderton",
+    version: "Paperback",
+    price: "$18.99",
+    genres: "Memoir, Biography, Autobiography, Humor",
+    img: "./book1.jpg",
+  },
+  {
+    title: "Good Material: A novel",
+    author: "Dolly Alderton",
+    version: "Hardcover",
+    price: "$28.00",
+    genres: "Humorous Fiction",
+    img: "./book2.jpg",
+  },
+  {
+    title: "A Litle Life",
+    author: "Hanya Yanagihara",
+    version: "Paperback",
+    price: "$18.00",
+    genres: "Novel",
+    img: "./book3.jpg",
+  },
+  {
+    title: "Bride",
+    author: "Ali Hazelwood",
+    version: "Paperback",
+    price: "$19.00",
+    genres: "Romance, Fantasy",
+    img: "./book4.jpg",
+  },
+  {
+    title: "A Court of Thorns and Roses (Book 1)",
+    author: "Sarah J. Maas",
+    version: "Paperback",
+    price: "$19.00",
+    genres: "Novel, Fantasy Fiction, Young adult literature, High fantasy",
+    img: "./book5.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book6.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book7.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book8.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book9.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book10.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book11.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book12.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book13.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book14.jpg",
+  },
+  {
+    title: "",
+    author: "",
+    version: "PaperbackHardcover",
+    price: "$",
+    genres: "",
+    img: "./book15.jpg",
+  },
+]
 </script>
 
 <style scoped>
