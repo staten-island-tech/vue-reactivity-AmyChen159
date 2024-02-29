8f91dd6c3e7d102b0c9c8fbee1093ae98d97e16f
AmyChen159 <144455940+AmyChen159@users.noreply.github.com>
2024-02-29 09:24:19 -0500

    heuueueueu

--- vue/src/components/BookCard.vue
+++ vue/src/components/BookCard.vue
@@ -3,7 +3,7 @@
         <h2>{{ Book.title }}</h2>
         <h3>By: {{ Book.author }}</h3>
         <img :src="Book.img" alt="" />
-        <h3>{{ clicked }}</h3>
+        <h3>{{ clicked }} Item(s) in Cart</h3>
         <button @click="increment">Add to Cart</button>
     </div>
 </template>
